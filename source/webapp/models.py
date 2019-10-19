9a5bb9ed99c632e622e62556fd68c223a0b2b011
k2Raven <leha.21.05.1996@gmail.com>
2019-10-19 13:11:44 +0600

    Создал модель Опроса (Poll), модель Варианта ответа (Choice) и Зарегистрировал модели в админ-панели и добавил тестовые данные, создал фикстуру

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.TextField(max_length=2000, null=False, blank=False, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    option = models.TextField(max_length=2500, null=False, blank=False, verbose_name='Текст варианта')
+    poll = models.ForeignKey('webapp.Poll', related_name='choice', on_delete=models.CASCADE, verbose_name='Опрос')
+
+    def __str__(self):
+        return self.option
+
