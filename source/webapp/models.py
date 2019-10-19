1c4f203b62909e8eb9ce1b0ca847d1686eaa0f03
k2Raven <leha.21.05.1996@gmail.com>
2019-10-19 17:01:03 +0600

    Сделал модел Answer и довавил ее в админку

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -16,3 +16,11 @@ class Choice(models.Model):
     def __str__(self):
         return self.option
 
+
+class Answer(models.Model):
+    poll = models.ForeignKey('webapp.Poll', related_name='answer', on_delete=models.CASCADE, verbose_name='Опрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    question = models.ForeignKey('webapp.Choice', related_name='answer', on_delete=models.CASCADE, verbose_name='Ответ')
+
+    def __str__(self):
+        return self.created_at
